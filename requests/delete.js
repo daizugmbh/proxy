485ab57071c218e95ca1a00c5a60463beaa48f5d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-19 15:42:27 +0200

    update delete request

--- requests/delete.js
+++ requests/delete.js
@@ -3,11 +3,12 @@ const axios = require("axios")
 exports.deleteData = async (req, res) => {
   try {
     const { server } = req.query
+    const { headers } = req
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Content-Type", "application/json")
     const response = await axios.delete(server)
-    const { headers, data } = await response
-    return res.status(200).json(data)
+    const { headers: responseHeaders, data: responseData } = await response
+    return res.status(200).json(responseData)
   } catch (error) {
     return res.status(500).json(error)
   }
