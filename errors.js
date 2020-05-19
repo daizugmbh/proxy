c36dc797ff4e8c16973e93fed91ce209f514fa51
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-19 15:44:13 +0200

    update errors.js

--- errors.js
+++ errors.js
@@ -1,17 +1,8 @@
-exports.wrongEndpoint = (req, res) => {
-  return res.status(404).json({
-    error: {
-      message:
-        "The endpoint you selected does not exist. Please refer the the manual.",
-    },
-  })
-}
-
 exports.serverInRequest = (req, res, next) => {
   if ("server" in req.query) {
     next()
   } else {
-    res.status(400).json({
+    return res.status(400).json({
       error: { message: "You have to provide a server in your request." },
     })
   }
@@ -19,12 +10,12 @@ exports.serverInRequest = (req, res, next) => {
 
 exports.wrongArgType = (req, res, next) => {
   const { server } = req.query
-  if (typeof server === "string") {
+  if (typeof server == "string") {
     next()
   } else {
-    res
+    return res
       .status(400)
-      .json({ error: { message: "The server has to be a string" } })
+      .json({ error: { message: "The server has to be a string." } })
   }
 }
 
@@ -33,12 +24,19 @@ exports.checkAddress = (req, res, next) => {
   if (server.startsWith("http://") || server.startsWith("https://")) {
     next()
   } else {
-    res
-      .status(400)
-      .json({
-        error: {
-          message: "The server has to start with 'http://' or 'https://'.",
-        },
-      })
+    return res.status(400).json({
+      error: {
+        message: "The server has to start with 'http://' or 'https://'.",
+      },
+    })
   }
+}
+
+exports.wrongEndpoint = (req, res) => {
+  return res.status(404).json({
+    error: {
+      message:
+        "The endpoint you selected does not exist. Please refer the the manual.",
+    },
+  })
 }
