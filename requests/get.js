f2dddc4d69cb6409893d2b6e4c06a954aaa25b75
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-19 15:47:20 +0200

    update get request

--- requests/get.js
+++ requests/get.js
@@ -3,11 +3,12 @@ const axios = require("axios")
 exports.getData = async (req, res) => {
   try {
     const { server } = req.query
+    const { headers } = req
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Content-Type", "application/json")
     const response = await axios.get(server)
-    const { headers, data } = await response
-    return res.status(200).json(data)
+    const { headers: responseHeaders, data: responseData } = await response
+    return res.status(200).json(responseData)
   } catch (error) {
     return res.status(500).json(error)
   }
